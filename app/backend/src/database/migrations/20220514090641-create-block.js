4793767e69fb459a4899e6c786451d4b91b1b712
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-14 07:27:47 -0300

    fix: user model name on create block

--- app/backend/src/database/migrations/20220514090641-create-block.js
+++ app/backend/src/database/migrations/20220514090641-create-block.js
@@ -17,7 +17,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
         references: {
-          model: "users",
+          model: "user",
           key: "id",
         },
       },
