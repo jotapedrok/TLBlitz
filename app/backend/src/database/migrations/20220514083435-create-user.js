7b909ad9d919433d8ed31892bac117432954ebd8
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-14 05:43:13 -0300

    create: User Model

---
+++ app/backend/src/database/migrations/20220514083435-create-user.js
@@ -0,0 +1,49 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
---
+++ app/backend/src/database/models/users.model.ts
@@ -0,0 +1,63 @@
+import { DATE, UUID, BOOLEAN, UUIDV4, STRING, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  public id!: number;
+
+  public username!: string;
+
+  public role!: string;
+
+  public email!: string;
+
+  public password: string;
+
+  public deleted: boolean;
+
+  public deletedAt: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: UUID,
+      defaultValue: UUIDV4,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    username: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    role: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    deleted: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+    deletedAt: {
+      type: DATE,
+      allowNull: true,
+    }
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'user',
+    timestamps: true,
+    tableName: 'users',
+  },
+);
+
+export default User;
