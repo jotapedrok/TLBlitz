bc749594feda81843550d686f88b25f8725000e0
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-17 17:51:02 -0300

    set: block routes(not implemented)

--- app/backend/src/routes.ts
+++ app/backend/src/routes.ts
@@ -9,4 +9,11 @@ routes.get('/users');
 routes.post('/users');
 routes.put('/users/:id');
 
+routes.get('/blocks');
+routes.get('/blocks/:userId');
+routes.post('/blocks');
+routes.post('/blocks/add/:userId');
+routes.put('/blocks/:id');
+
+
 export default routes;
