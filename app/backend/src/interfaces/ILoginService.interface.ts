6c9248da43ad4cac95e9817af08f50f0c5f4d0bc
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-17 18:43:17 -0300

    fix: IModel

--- app/backend/src/interfaces/ILoginService.interface.ts
+++ app/backend/src/interfaces/ILoginService.interface.ts
@@ -1,7 +1,9 @@
+import { IModel } from "./IModel.interface";
 import { IServiceResponse } from "./IServiceResponse.interface";
 import { IUser } from "./IUser.interface";
 
 export interface ILoginService {
+  userModel: IModel;
   genToken(payload: IUser): string;
   authorization(payload: IUser): IServiceResponse;
   authentification(token: string): IServiceResponse;
--- app/backend/src/interfaces/IModel.interface.ts
+++ app/backend/src/interfaces/IModel.interface.ts
@@ -1,12 +1,8 @@
+import { Model } from "sequelize/types";
+
 export interface Entity {
   id?: string,
 }
 
-export interface IModel<T extends Entity> {
-  findByPk(id: string): Promise<T>
-  findAll(): Promise<T[]>
-  findOne(props: any): Promise<T>
-  create(obj: T): Promise<T>
-  update(obj: T): Promise<void>
-  delete(id: number): Promise<void>
+export interface IModel extends Model {
 }
