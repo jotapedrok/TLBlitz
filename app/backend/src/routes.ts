7452f413133305ae94399ce9f84050405189d0f7
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-18 17:19:42 -0300

    implements: block controllers on routes

--- app/backend/src/routes.ts
+++ app/backend/src/routes.ts
@@ -1,15 +1,22 @@
 import { Router } from 'express';
+import { BlockController } from './controllers/block.controller';
 import UserController from './controllers/user.controller';
+import Block from './database/models/blocks.model';
 import User from './database/models/users.model';
 import FieldsValidation from './middlewares/FieldsValidation';
 import { LoginValidation } from './middlewares/LoginValidation.middleware';
 import { UserValidation } from './middlewares/UserValidation.middleware';
 import { AuthService } from './services/auth.service';
+import { BlockService } from './services/block.service';
 import { UserService } from './services/user.service';
 
 const authService = new AuthService(User);
 const userService = new UserService(User);
+const blockService = new BlockService(Block, User);
+
 const userController = new UserController(authService, userService);
+const blockController = new BlockController(blockService);
+
 const loginValidations = new LoginValidation(FieldsValidation);
 const userValidations = new UserValidation(FieldsValidation);
 
@@ -22,11 +29,11 @@ routes.get('/users', userController.getAll);
 routes.post('/users', userValidations.create, userController.create);
 routes.patch('/users/:id', userValidations.update, userController.edit);
 
-routes.get('/blocks/:userId');
-routes.get('/blocks');
-routes.post('/blocks');
-routes.patch('/blocks/add/:userId/:blockId');
-routes.patch('/blocks/:id');
+routes.get('/blocks/:userId', blockController.getAllByUserId);
+routes.get('/blocks', blockController.getAll);
+routes.post('/blocks', blockController.create);
+routes.patch('/blocks/add/:userId/:blockId', blockController.addUser);
+routes.patch('/blocks/:id', blockController.edit);
 
 routes.get('/tasks');
 routes.get('/tasks/:blockId');
