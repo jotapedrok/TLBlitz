8158a19dea9911f24a64298800718f6bb14d2168
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-18 16:53:03 -0300

    set: blocks getAllByUserId service

---
+++ app/backend/src/services/block.service.ts
@@ -0,0 +1,15 @@
+import { IModel } from "../interfaces/IModel.interface";
+import { IBlockService } from "../interfaces/IBlockService.interface";
+import { IServiceResponse } from "../interfaces/IServiceResponse.interface";
+
+export class BlockService implements IBlockService {
+  constructor(private blockModel: IModel) { }
+
+  async getAllByUserId(userId: string): Promise<IServiceResponse> {
+    const blocks = this.blockModel.findOne({ where: { createdBy: userId }, raw: true });
+    if (!blocks) {
+      return { error: 'No one block was found', data: [] };
+    }
+    return { error: false, data: blocks };
+  }
+}
