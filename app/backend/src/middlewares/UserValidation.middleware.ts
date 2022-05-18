23c07b39d74955490c73009d6494fb261ebb3668
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-18 15:38:51 -0300

    set: userValidations - create

---
+++ app/backend/src/middlewares/UserValidation.middleware.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from "express";
+import { IFieldsValidations } from "../interfaces/IFieldsValidation.interface";
+
+export class UserValidation {
+  constructor(private fieldsValidations: IFieldsValidations) { }
+  create: RequestHandler = (req, res, next) => {
+    try {
+      const { email, username, password } = req.body;
+      const testEmail = this.fieldsValidations.email(email);
+      const testPassword = this.fieldsValidations.password(password);
+      const testUsername = this.fieldsValidations.username(username);
+      if (!testEmail.test) {
+        return res.status(400).json({ error: testEmail.message });
+      }
+      if (!testPassword.test) {
+        return res.status(400).json({ error: testPassword.message });
+      }
+      if (!testUsername.test) {
+        return res.status(400).json({ error: testUsername.message });
+      }
+      next();
+    } catch (e) {
+      next(e);
+    }
+  };
+}
--- app/backend/src/routes.ts
+++ app/backend/src/routes.ts
@@ -3,22 +3,23 @@ import UserController from './controllers/user.controller';
 import User from './database/models/users.model';
 import FieldsValidation from './middlewares/FieldsValidation';
 import { LoginValidation } from './middlewares/LoginValidation.middleware';
+import { UserValidation } from './middlewares/UserValidation.middleware';
 import { AuthService } from './services/auth.service';
 import { UserService } from './services/user.service';
 
 const authService = new AuthService(User);
 const userService = new UserService(User);
 const userController = new UserController(authService, userService);
-
-const validationLogin = new LoginValidation(FieldsValidation);
+const loginValidations = new LoginValidation(FieldsValidation);
+const userValidations = new UserValidation(FieldsValidation);
 
 const routes = Router();
 
-routes.post('/login', validationLogin.login, userController.login);
+routes.post('/login', loginValidations.login, userController.login);
 
 routes.get('/users/:id', userController.getById);
 routes.get('/users', userController.getAll);
-routes.post('/users', userController.create);
+routes.post('/users', userValidations.create, userController.create);
 routes.patch('/users/:id', userController.edit);
 
 routes.get('/blocks');
