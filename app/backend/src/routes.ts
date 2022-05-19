fcc151460584622811330784af13228ae8ea6d2a
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-19 04:36:43 -0300

    implements: taskController on routes

--- app/backend/src/routes.ts
+++ app/backend/src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { BlockController } from './controllers/block.controller';
+import { TaskController } from './controllers/task.controller';
 import UserController from './controllers/user.controller';
 import Block from './database/models/blocks.model';
+import Status from './database/models/status.model';
+import Task from './database/models/task.model';
 import User from './database/models/users.model';
 import UserBlocks from './database/models/usersBlocks.model';
 import { BlockValidation } from './middlewares/BlockValidation.middleware';
@@ -10,14 +13,17 @@ import { LoginValidation } from './middlewares/LoginValidation.middleware';
 import { UserValidation } from './middlewares/UserValidation.middleware';
 import { AuthService } from './services/auth.service';
 import { BlockService } from './services/block.service';
+import { TaskService } from './services/task.service';
 import { UserService } from './services/user.service';
 
 const authService = new AuthService(User);
 const userService = new UserService(User);
 const blockService = new BlockService(Block, User, UserBlocks);
+const taskService = new TaskService(Task, User, Block, Status);
 
 const userController = new UserController(authService, userService);
 const blockController = new BlockController(blockService);
+const taskController = new TaskController(taskService);
 
 const loginValidations = new LoginValidation(FieldsValidation);
 const userValidations = new UserValidation(FieldsValidation);
@@ -40,12 +46,12 @@ routes.patch('/blocks/edit/:userId/:blockId', blockValidations.editUser, blockCo
 routes.patch('/blocks/delete/:userId/:blockId', blockController.deleteUser);
 routes.patch('/blocks/:id', blockValidations.edit, blockController.edit);
 
-routes.get('/tasks');
-routes.get('/tasks/:userId');
-routes.get('/tasks/:blockId');
-routes.post('/tasks');
-routes.patch('/tasks/:id');
-routes.patch('/tasks/status/:id');
+routes.get('/tasks', taskController.getAll);
+routes.get('/tasks/:userId', taskController.getAllByUserId);
+routes.get('/tasks/:blockId', taskController.getAllByBlockId);
+routes.post('/tasks', taskController.create);
+routes.patch('/tasks/:id', taskController.edit);
+routes.patch('/tasks/status/:id', taskController.changeStatus);
 
 
 routes.get('/status');
