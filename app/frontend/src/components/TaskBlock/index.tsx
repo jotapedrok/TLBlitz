97f513742ad30c604bd0fcadbcd59a60d5276d68
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-19 18:28:38 -0300

    create: components Desktop (not implemented)

--- app/frontend/src/components/TaskBlock/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './style.scss';
-
-export default function TaskBlock() {
-  return <div>TaskBlock</div>;
-}
---
+++ app/frontend/src/components/TaskList/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.scss';
+
+export default function TaskList() {
+  return <div>TaskList</div>;
+}
