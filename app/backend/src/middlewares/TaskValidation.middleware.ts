0cfcdaeffe732179c5b2eb8225e8cec23daae380
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-19 05:04:59 -0300

    implements: TaskValidation edit

--- app/backend/src/middlewares/TaskValidation.middleware.ts
+++ app/backend/src/middlewares/TaskValidation.middleware.ts
@@ -39,6 +39,19 @@ export class BlockValidation {
 
   edit: RequestHandler = (req, res, next) => {
     try {
+      const { title, content, description } = req.body;
+      const testTitle = this.fieldsValidations.title(title);
+      const testContent = this.fieldsValidations.content(content);
+      const testDescription = this.fieldsValidations.description(description);
+      if (!testTitle.test) {
+        return res.status(400).json({ error: testTitle.message });
+      }
+      if (!testContent.test) {
+        return res.status(400).json({ error: testContent.message });
+      }
+      if (!testDescription.test) {
+        return res.status(400).json({ error: testDescription.message });
+      }
       next();
     } catch (e) {
       next(e);
