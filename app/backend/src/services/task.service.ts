c8a17700d8f91fe0394dd9ea228ae30b87fbe273
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-19 04:08:01 -0300

    implements: taskService getAllByUserId & getAllByBlockId

--- app/backend/src/services/task.service.ts
+++ app/backend/src/services/task.service.ts
@@ -4,24 +4,35 @@ import { ITask } from "../interfaces/ITask.interface";
 import { editableFields, ITaskService } from "../interfaces/ITaskService.interface";
 
 export class TaskService implements ITaskService {
-  constructor(private taskModel: IModel) { };
+  constructor(private taskModel: IModel, private userModel: IModel, private blockModel: IModel) { };
 
   getAll = async (): Promise<IServiceResponse> => {
     const tasks = await this.taskModel.findAll();
     return { error: false, data: tasks };
   };
+
   getAllByUserId = async (userId: string): Promise<IServiceResponse> => {
-    return { error: false, data: 'DEFAULT' };
+    const found = await this.userModel.findByPk(userId);
+    if (!found) return { error: 'User not found' };
+    const tasks = await this.taskModel.findAll({ where: { createdBy: userId } });
+    return { error: false, data: tasks };
   };
+
   getAllByBlockId = async (blockId: string): Promise<IServiceResponse> => {
-    return { error: false, data: 'DEFAULT' };
+    const found = await this.blockModel.findByPk(blockId);
+    if (!found) return { error: 'Block not found' };
+    const tasks = await this.taskModel.findAll({ where: { createdBy: blockId } });
+    return { error: false, data: tasks };
   };
+
   create = async (task: ITask): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
   };
+
   edit = async (id: string, payload: editableFields): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
   };
+
   changeStatus = async (id: string, status: string): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
   };
