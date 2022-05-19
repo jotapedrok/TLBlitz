652bc8fdcb392f2deec132041426b9efe31fbb08
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-19 04:04:00 -0300

    implements: taskService getAll

--- app/backend/src/services/task.service.ts
+++ app/backend/src/services/task.service.ts
@@ -4,23 +4,25 @@ import { ITask } from "../interfaces/ITask.interface";
 import { editableFields, ITaskService } from "../interfaces/ITaskService.interface";
 
 export class TaskService implements ITaskService {
-  constructor(private taskModel: IModel) { }
+  constructor(private taskModel: IModel) { };
+
   getAll = async (): Promise<IServiceResponse> => {
-    return { error: false, data: 'DEFAULT' };
-  }
+    const tasks = await this.taskModel.findAll();
+    return { error: false, data: tasks };
+  };
   getAllByUserId = async (userId: string): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
-  }
+  };
   getAllByBlockId = async (blockId: string): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
-  }
+  };
   create = async (task: ITask): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
-  }
+  };
   edit = async (id: string, payload: editableFields): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
-  }
+  };
   changeStatus = async (id: string, status: string): Promise<IServiceResponse> => {
     return { error: false, data: 'DEFAULT' };
-  }
+  };
 }
