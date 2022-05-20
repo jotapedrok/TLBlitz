c0b529fdb465ce9cb2201cfedf6faeacfe1cb6c5
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-20 02:14:14 -0300

    set: authorization in ProtectedRoute

--- app/frontend/src/pages/Signin/index.tsx
+++ app/frontend/src/pages/Signin/index.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../services/auth';
+import { authenticate } from '../../store/user.store';
 import './style.scss';
 
 export default function Signin() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const authorization = async () => {
+    const token = localStorage.getItem('TLBTK_');
+    if (token) {
+      const authTest = await auth(token);
+      if (authTest) {
+        dispatch(authenticate(token));
+        navigate('/');
+      }
+    }
+  };
+
+  useEffect(() => {
+    authorization();
+  }, []);
+
   return <div>Signin</div>;
 }
