cddda7ef6ee09a1fa9f403d364a5e33483d73d5d
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-20 02:04:50 -0300

    create: auth service

--- app/frontend/src/pages/Login/index.tsx
+++ app/frontend/src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../../services/auth';
 import { authenticate } from '../../store/user.store';
 import './style.scss';
 
@@ -8,17 +9,20 @@ export default function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const auth = () => {
+  const authorization = async () => {
     const token = localStorage.getItem('TLBTK_');
     if (token) {
-      //testAuthentication
-      dispatch(authenticate(token));
-      navigate('/');
+      const authTest = await auth(token);
+      if (authTest) {
+        dispatch(authenticate(token));
+        navigate('/');
+
+      }
     }
-  }
+  };
 
   useEffect(() => {
-    auth();
+    authorization();
   }, []);
   return <div>Login</div>;
 }
--- app/frontend/src/services/api.ts
+++ app/frontend/src/services/api.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
+import store from "../store";
+
+const { user: { token } } = store.getState();
 
 const api = axios.create({
   baseURL: process.env.API_URL,
   responseType: 'json',
+  headers: {
+    Authorization: token
+  }
 })
 
 export { api }
