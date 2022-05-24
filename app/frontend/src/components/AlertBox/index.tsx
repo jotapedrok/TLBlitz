9f7c99021c278ce57bbd660b4965ccd5d61b015c
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-24 20:08:08 -0300

    implements: AlertBox (not stylezed)

--- app/frontend/src/components/AlertBox/index.tsx
+++ app/frontend/src/components/AlertBox/index.tsx
@@ -1,6 +1,57 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { MdClose } from 'react-icons/md';
 import './style.scss';
 
-export default function AlertBox() {
-  return <div className="AlertBox">AlertBox</div>;
+interface props {
+  hasButton: boolean;
+  buttons: {
+    i: number;
+    text: string;
+    onClick(): void;
+    variant: string;
+  }[];
+  content: string;
+  title: string;
+}
+
+export default function AlertBox({
+  hasButton,
+  buttons,
+  content,
+  title,
+}: props) {
+  return (
+    <div className="alert-box">
+      <div className="alert-box-header">
+        <h5 className="alert-box-header-title">{title}</h5>
+        {!hasButton && (
+          <div className="alert-box-header-close">
+            <MdClose />
+          </div>
+        )}
+      </div>
+      <hr className="line" />
+      <div className="content-container">
+        <p>{content}</p>
+      </div>
+      {hasButton && (
+        <>
+          <hr />
+          <div className="buttons-conteiner">
+            {buttons.map(button => (
+              <Button
+                type="button"
+                key={`${button.i}_btn_key_define`}
+                onClick={button.onClick}
+                variant={button.variant}
+              >
+                {button.text}
+              </Button>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
