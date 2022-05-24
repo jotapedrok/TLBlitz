d401a4295185be90addd065c22490d903d8ceeb1
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-24 20:25:48 -0300

    implements: alertBox on App.tsx

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
+import AlertBox from './components/AlertBox';
 import Router from './routes/routes';
+import { RootState } from './store';
 
 function App() {
+  const { hasAlert, alert } = useSelector((s: RootState) => s.alert);
   return (
     <div className="App">
+      {hasAlert && (
+        <AlertBox
+          buttons={alert.buttons || []}
+          content={alert.content || ''}
+          title={alert.title || ''}
+          hasButton={alert.hasButton || false}
+        />
+      )}
       <Router />
     </div>
   );
