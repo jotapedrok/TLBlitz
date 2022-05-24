6c731002230910c391c959aaaf5240bdcd0be384
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-24 17:14:41 -0300

    set: filter bar TaskBlock

--- app/frontend/src/pages/Home/index.tsx
+++ app/frontend/src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormControl, FormSelect, InputGroup } from 'react-bootstrap';
 import Header from '../../components/Header';
 import TaskBlock from '../../components/TaskBlock';
 import './style.scss';
@@ -11,7 +12,22 @@ export default function Home() {
         <div className="title-one">
           <h4>Your Blocks:</h4>
         </div>
-        <TaskBlock name="test" group={false} />
+        <div className="filter-bar-container">
+          <InputGroup className="mb-3">
+            <FormControl
+              aria-label="filter-by-name"
+              placeholder="Filter Block Name"
+            />
+            <FormSelect>
+              <option value="all">All</option>
+              <option value="alone">Alone</option>
+              <option value="group">Group</option>
+            </FormSelect>
+          </InputGroup>
+        </div>
+        <div className="block-list">
+          <TaskBlock name="test" group={false} />
+        </div>
       </div>
     </div>
   );
