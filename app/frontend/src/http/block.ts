eeb60235f951619e6be62aa14d94d59baa7d3f95
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-24 18:44:02 -0300

    create: http blocks post Block

--- app/frontend/src/http/block.ts
+++ app/frontend/src/http/block.ts
@@ -1,6 +1,7 @@
-import { api } from "../services/api";
+import { IBlock } from '../interfaces/IBlock.interface';
+import { api } from '../services/api';
 
 export async function createBlock(Block: IBlock) {
-  const response = await api.post('/Blocks', Block);
+  const response = await api.post('/blocks', Block);
   return response.data;
 }
