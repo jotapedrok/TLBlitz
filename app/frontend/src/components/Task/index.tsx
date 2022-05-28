ddadebd17af7d969bdc8846a04cf4a378046c33a
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-28 02:37:00 -0300

    implements: colapse content on Task

--- app/frontend/src/components/Task/index.tsx
+++ app/frontend/src/components/Task/index.tsx
@@ -1,6 +1,55 @@
-import React from 'react';
+import moment from 'moment';
+import React, { useState } from 'react';
+import { Collapse } from 'react-bootstrap';
 import './style.scss';
 
-export default function Task() {
-  return <div>Task</div>;
+interface props {
+  taskTitle: string;
+  taskStatus: string;
+  taskContent: string;
+  createdBy: string;
+  createdAt: number;
+  taskDescription: string;
+}
+
+export default function Task({
+  taskStatus,
+  taskTitle,
+  taskDescription,
+  taskContent,
+  createdBy,
+  createdAt,
+}: Partial<props>) {
+  const [open, setOpen] = useState(false);
+  const createdDate = moment(createdAt).format('DD/MM/yyyy');
+  return (
+    <div className="task">
+      <div className="task-header">
+        <div className="task-header-title">{taskTitle}</div>
+        <div className="task-header-status">{taskStatus}</div>
+      </div>
+      <div className="task-description">{taskDescription}</div>
+      <div className="task-options">
+        <div className="taks-options-info">
+          <p className="task-options-info-creator">{createdBy}</p>
+          <p className="task-options-info-created-at">{createdDate}</p>
+        </div>
+        <button type="button" className="btn task-options-edit">
+          edit
+        </button>
+        <button
+          onClick={() => setOpen(!open)}
+          aria-controls="example-collapse-text"
+          aria-expanded={open}
+          type="button"
+          className="btn task-options-open-content"
+        >
+          open
+        </button>
+      </div>
+      <Collapse in={open}>
+        <div className="task-content">{taskContent}</div>
+      </Collapse>
+    </div>
+  );
 }
--- app/frontend/src/components/TaskList/index.tsx
+++ app/frontend/src/components/TaskList/index.tsx
@@ -2,11 +2,28 @@ import React from 'react';
 import Task from '../Task';
 import './style.scss';
 
+const content =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure illo assumenda consequuntur dicta ducimus amet vero voluptates nostrum! Iure dolorem unde natus! Nisi illo dolorum rerum tenetur voluptatum sunt Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo iure illo assumenda consequuntur dicta ducimus amet vero voluptates nostrum! Iure dolorem unde natus! Nisi illo dolorum rerum tenetur voluptatum sunt ';
+
 export default function TaskList() {
   return (
     <div className="TaskList">
-      <Task />
-      <Task />
+      <Task
+        taskStatus="coisado"
+        taskTitle="Título 1"
+        taskContent={content}
+        taskDescription="Fazer a parada e tal, sas coisa toda..."
+        createdAt={1653715538000}
+        createdBy="Muá mesmo"
+      />
+      <Task
+        taskStatus="descoisado"
+        taskTitle="Título 2"
+        taskContent={content}
+        taskDescription="Fazer a parada e tal, sas coisa toda dnv..."
+        createdAt={1653715538000}
+        createdBy="Muá mesmo"
+      />
     </div>
   );
 }
