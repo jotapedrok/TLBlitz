0dab0c118b53acf90b571c0db911c19c8ab0b895
Jota Aguiar <jotapedrok@pop-os.localdomain>
2022-05-30 13:07:46 -0300

    set: handleChange

--- app/frontend/src/components/EditTask/index.tsx
+++ app/frontend/src/components/EditTask/index.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, FormControl, InputGroup } from 'react-bootstrap';
 import './style.scss';
 
 export default function EditTask() {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [content, setContent] = useState('');
+  const [inputValues, setInputValues] = useState({
+    title: '',
+    description: '',
+    contente: '',
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setInputValues({
+      ...inputValues,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="edit-task">
       <div className="edit-task-x-btn">x</div>
@@ -14,6 +26,9 @@ export default function EditTask() {
         <div className="edit-task-title-container-pen-btn">pen</div>
         <InputGroup>
           <FormControl
+            value={inputValues.title}
+            onChange={handleChange}
+            name="title"
             placeholder="Task Title"
             aria-label="task-title"
             aria-describedby="basic-addon1"
@@ -28,7 +43,13 @@ export default function EditTask() {
         </div>
         <div className="edit-task-description-container-pen-btn">pen</div>
         <InputGroup>
-          <FormControl as="textarea" aria-label="description textarea" />
+          <FormControl
+            onChange={handleChange}
+            value={inputValues.description}
+            name="description"
+            as="textarea"
+            aria-label="description textarea"
+          />
           <InputGroup.Text id="basic-addon1">Save Icon</InputGroup.Text>
           <Button variant="light">x</Button>
         </InputGroup>
@@ -37,7 +58,13 @@ export default function EditTask() {
         <div className="edit-task-content-container-content">Content</div>
         <div className="edit-task-content-container-pen-btn">pen</div>
         <InputGroup>
-          <FormControl as="textarea" aria-label="content textarea" />
+          <FormControl
+            onChange={handleChange}
+            value={inputValues.contente}
+            name="content"
+            as="textarea"
+            aria-label="content textarea"
+          />
           <InputGroup.Text id="basic-addon1">Save Icon</InputGroup.Text>
           <Button variant="light">x</Button>
         </InputGroup>
