915dad7ff538b74fdabde8dc9abc9273dcbfcc0e
Jota Aguiar <jotapedrok@gmail.com>
2022-06-03 21:47:12 -0300

    set: alert on fetch task error

--- app/frontend/src/components/TaskList/index.tsx
+++ app/frontend/src/components/TaskList/index.tsx
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { getTasks } from '../../http/task';
 import { ITask } from '../../interfaces/ITask.interface';
+import {
+  activeAlert,
+  desativeAlert,
+  resetAlert,
+  sendAlert,
+} from '../../store/alert.store';
+import { IAlertProps } from '../AlertBox';
 import Task from '../Task';
 import './style.scss';
 
@@ -32,10 +40,29 @@ interface props {
 
 export default function TaskList({ blockId }: Partial<props>) {
   const [tasks, setTasks] = useState(initialTasks);
+  const dispatch = useDispatch();
   const fetchTasks = async () => {
     const response = await getTasks(blockId || '');
     if (response.error) {
-      console.log(response.erro);
+      const alert: IAlertProps = {
+        hasButton: true,
+        title: 'Error on server',
+        content: response.error,
+        buttons: [
+          {
+            id: '7',
+            text: 'Ok',
+            variant: 'secondary',
+            onClick: e => {
+              e.preventDefault();
+              dispatch(desativeAlert());
+              dispatch(resetAlert());
+            },
+          },
+        ],
+      };
+      dispatch(sendAlert(alert));
+      dispatch(activeAlert());
     } else {
       setTasks(response.data);
     }
